78a52505f55289367d07bb179e21155da9ab371b
gioboa <giorgiob.boa@gmail.com>
2023-05-09 15:11:12 +0200

    linter

--- host/src/main.ts
+++ host/src/main.ts
@@ -1,12 +1,12 @@
-import './app.postcss';
-import { initFederation } from '@softarc/native-federation';
+import "./app.postcss";
+import { initFederation } from "@softarc/native-federation";
 
 (async () => {
-	const response = await fetch('/mfes-config.json');
+  const response = await fetch("/mfes-config.json");
   const mfesConfig = await response.json();
-  console.log('CONFIG:', mfesConfig);
-  
-	await initFederation(mfesConfig);
+  console.log("CONFIG:", mfesConfig);
 
-	await import('./bootstrap');
+  await initFederation(mfesConfig);
+
+  await import("./bootstrap");
 })();
